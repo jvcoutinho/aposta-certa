fcfe6a74c2e5b92eb402c4fa17cf3e26e7f13c9e
João Victor <joao.coutinho9@gmail.com>
2018-06-16 23:30:32 -0300

    Teste de aceitação da ordenação de apostas

--- tests-acceptance/step-definitions/listaApostas.ts
+++ tests-acceptance/step-definitions/listaApostas.ts
@@ -3,37 +3,69 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
-let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
-
-let sameCPF = ((elem, cpf) => elem.element(by.name('cpflist')).getText().then(text => text === cpf));
-let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
-
-let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+var indexMandante = -1, indexVisitante = -1;
 
 defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^“Flamengo” versus “Botafogo” is on the list of bets$/, async () => {
-        await browser.get("http://localhost:4200/");
-        await expect(browser.getTitle()).to.eventually.equal('ApostaCerta');
-        //await $("a[name='alunos']").click();
+
+    Given(/^I\'m at "([^\"]*)" page$/, async(page) => {
+        await browser.get("http://localhost:4200/concurso");
+        await expect(browser.getTitle()).to.eventually.equal('ApostaCerta');           
+    });
+
+    Given(/^"([^\"]*)" versus "([^\"]*)" is on the list of bets$/, async (mandante, visitante) => {
+        var allMandantes: ElementArrayFinder = element.all(by.className('mandante'));
+        await allMandantes;
+        var allVisitantes: ElementArrayFinder = element.all(by.className('visitante'));
+        await allVisitantes;
+        await allMandantes.each((item, index) => item.getText().then(text => { 
+            if(text === mandante) 
+                indexMandante = index; 
+            }));
+        await allVisitantes.each((item, index) => item.getText().then(text => { 
+            if(text === visitante) 
+                indexVisitante = index; 
+            }));
+        await expect(Promise.resolve(indexMandante)).to.eventually.not.equal(-1);
+        await expect(Promise.resolve(indexMandante)).to.eventually.equal(indexVisitante);
     })
 
-    Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
-        var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
-        await allcpfs;
-        var samecpfs = allcpfs.filter(elem =>
-                                      elem.getText().then(text => text === cpf));
-        await samecpfs;
-        await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    Given(/^"([^\"]*)" versus "([^\"]*)" will happen "([^\"]*)"$/, async (mandante, visitante, data) => {
+        var allDatas: ElementArrayFinder = element.all(by.className('data'));
+        var indexData = -1;
+        
+        await allDatas.get(indexMandante).getText().then(text => {
+            if(text === data)
+                indexData = indexMandante;
+        });
+        await expect(Promise.resolve(indexData)).to.eventually.not.equal(-1);
     });
 
-    When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-        await $("input[name='namebox']").sendKeys(<string> name);
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await element(by.buttonText('Adicionar')).click();
+    When(/^I sort the list by date$/, async () => {
+        
     });
 
-    Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
-        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-        allalunos.filter(elem => pAND(sameCPF(elem,cpf),sameName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    Then(/^I see "([^\"]*)" versus "([^\"]*)" before "([^\"]*)" versus "([^\"]*)" on the list$/, async (mandante1, visitante1, mandante2, visitante2) => {
+        var allMandantes: ElementArrayFinder = element.all(by.className('mandante'));
+        await allMandantes;
+        var allVisitantes: ElementArrayFinder = element.all(by.className('visitante'));
+        await allVisitantes;
+
+        var indexMandante1, indexMandante2;
+        var indexVisitante1, indexVisitante2;
+
+        await allMandantes.each((item, index) => item.getText().then(text => { 
+            if(text === mandante1) 
+                indexMandante1 = index;
+            else if(text === mandante2)
+                indexMandante2 = index; 
+            }));
+        await allVisitantes.each((item, index) => item.getText().then(text => { 
+            if(text === visitante1) 
+                indexVisitante1 = index;
+            else if(text === visitante2)
+                indexVisitante2 = index; 
+            }));
+        await expect(Promise.resolve(indexMandante1)).to.eventually.lessThan(indexMandante2); 
+        await expect(Promise.resolve(indexVisitante1)).to.eventually.lessThan(indexVisitante2); 
     });
 })
