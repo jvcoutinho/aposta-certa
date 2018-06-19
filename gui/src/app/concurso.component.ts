9fbccffa686e6afe5d8b9155d63e6ced26f533a5
João Victor <joao.coutinho9@gmail.com>
2018-06-19 13:38:40 -0300

    Implementação da ordenação de apostas

--- gui/src/app/concurso.component.ts
+++ gui/src/app/concurso.component.ts
@@ -15,6 +15,29 @@ export class ConcursoComponent implements OnInit {
 
   public apostas: Aposta[];
 
+  sortList(): void {
+    let apostasOrdenadas: Aposta[] = this.apostas;
+    apostasOrdenadas.sort(this.compare);
+  }
+
+  private compare = function(a, b): number {
+    let days = ['Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo', 'Segunda-feira', 'Terça-feira'];
+    let aIndex, bIndex;
+    for(let i = 0; i < days.length; i++) {
+      if(days[i] === a.data) {
+          aIndex = i;
+          break;
+      }
+    }
+    for(let i = 0; i < days.length; i++) {
+      if(days[i] === b.data) {
+          bIndex = i;
+          break;
+      }
+    }
+    return aIndex - bIndex;
+  }
+
   ngOnInit(): void {
     this.apostasService.getApostas()
     .then(apostas => {console.log('Apostas', apostas); this.apostas = apostas})
