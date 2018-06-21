5821e6e4a059dbae35ccb4d0329f6bbde08823f4
Josenildo Vicente de Araujo <jva@ug3c16.windows.cin.ufpe.br>
2018-06-21 18:16:46 -0300

    "Implementação da proposta de marcação simples"

--- gui/src/app/marcacao.component.ts
+++ gui/src/app/marcacao.component.ts
@@ -10,32 +10,24 @@ import { ApostadorService } from './apostador.service';
 export class MarcacaoComponent implements OnInit {
 
   public constructor(private apostasService: ApostadorService) {
-      this.apostas = [];
+      this.propostas = [];
   }
 
-  public apostas: Aposta[];
-
-  sortList(): void {
-    this.apostas.sort(this.compare);
-  }
-
-  private compare = function(a, b): number {
-    let days = ['Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo', 'Segunda-feira', 'Terça-feira'];
-    return findIndex(days, a.data) - findIndex(days, b.data);
-  }
+  public propostas: Proposta[];
 
   ngOnInit(): void {
-    this.apostasService.getApostas()
-    .then(apostas => {console.log('Apostas', apostas); this.apostas = apostas})
+    this.apostasService.getPropostas()
+    .then(propostas => {console.log('Propostas', propostas); this.propostas = propostas})
     .catch(e => console.log('Erro: ' + e));
   }
 
-}
-
-function findIndex(array: any[], element: any): number {
-  for(let i = 0; i < array.length; i++) {
-    if(array[i] === element) {
-        return i;
+  corPropostas(id, prop){
+    let square = document.getElementById(id);
+    if(prop){
+      square.style.background = 'red';
+    }else{
+      square.style.background = 'white';
     }
   }
+
 }
